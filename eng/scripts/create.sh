dac4662b8a5b43753763170c737c216c1a0cdee1
Lyon Till <lyon.till@microsoft.com>
2024-06-19 11:08:35 +0100

    chore: remove debug argument

--- eng/scripts/create.sh
+++ eng/scripts/create.sh
@@ -4,7 +4,6 @@ az stack sub create \
     --name 'Microsoft.Patterns' \
     --template-file './src/main.bicep' \
     --parameters './src/parameters/main.bicepparam' \
-    --deny-settings-mode 'none' \
     --action-on-unmanage 'deleteAll' \
-    --yes \
-    --debug
+    --deny-settings-mode 'denyWriteAndDelete' \
+    --yes
